708c61571ec02c814e4ee2caf8193c724156a3b3
Oren Gurfinkel <oren.gurfinkel@hpe.com>
2018-09-26 23:04:31 +0300

    Introduce routes builder

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const serverFactory = require("./lib/server/serverFactory");
 const routesBuilder = require("./lib/server/routesBuilder");
+const publishAdminRoutes = require("./lib/server/adminRoutes");
+const publishExecutionRoutes = require("./lib/server/executionRoutes");
+const FlowManager = require("./lib/execution/flowManager");
 
 async function createServer(serverContext) {
-    const {server} = await serverFactory.createServerFromConfig(serverContext);
+    const {
+        server
+    } = await serverFactory.createServerFromConfig(serverContext);
     console.log(`Server created`);
 }
 
-createServer({flavor: "express", routes: routesBuilder.buildRoutes(), port: 3000});
+createServer({
+    flavor: "express",
+    routes: routesBuilder.buildRoutes([publishAdminRoutes(), publishExecutionRoutes(new FlowManager())]),
+    port: 3000
+});
